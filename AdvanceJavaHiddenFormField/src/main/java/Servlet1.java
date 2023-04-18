04396a46bafebae058f67964047aa3223de8bc04
OnlineTraning <57015977+Onlinetrainingtech@users.noreply.github.com>
2023-04-18 07:51:48 +0530

    18-04-2023

--- AdvanceJavaHiddenFormField/src/main/java/Servlet1.java
+++ AdvanceJavaHiddenFormField/src/main/java/Servlet1.java
@@ -3,14 +3,11 @@
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class Servlet1
@@ -21,23 +18,17 @@ public class Servlet1 extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
+		resp.setContentType("text/html");
 		PrintWriter out=resp.getWriter();
-		String uname=req.getParameter("uname");
-		String pass=req.getParameter("pass");
+		String e1=req.getParameter("email");
+		out.println("<form action='Servlet2' method='get'>");
 		
-		if(uname.equals("admin")&&pass.equals("admin"))
-		{
-			HttpSession sess=req.getSession();
-			sess.setAttribute("u1",uname);
-			resp.sendRedirect("Servlet2");
-		}
-		else
-		{
-			Cookie ck=new Cookie("u1",uname);
-			resp.addCookie(ck);
-			RequestDispatcher rd=req.getRequestDispatcher("Servlet3");
-			rd.forward(req, resp);
-		}
+		out.println("<input type='hidden' name='em' value='"+e1+"'/>");
+		
+		out.println("<input type='submit' value='ClickHere'/>");
+		
+		out.println("</form>");
+		
 	}
 
 }
